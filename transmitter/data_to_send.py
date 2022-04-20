d67ffd156d73f80e016c066cf7b480523c1f542c
Michelle Davies <mdd94@cornell.edu>
2022-04-20 18:49:46 -0400

    Update data_to_send.py

--- transmitter/data_to_send.py
+++ transmitter/data_to_send.py
@@ -1,6 +1,9 @@
 # function to read, format, and return the sensor data that we need
 
-## Import modules (gpio, libraries for reading data from sensors)
+## Import modules (gpio, libraries for reading data from sensors, etc)
+import time
+import sys
+import datetime
 
 def calibration_light():
   ## The transmitter is outputting a calibration light signal to indicate that data is being transmitted to the receiver.
@@ -13,14 +16,18 @@ def temp_and_hum_capture():
   
 
 def captureData():
-  ## formatting the data into a JSON -> work with dictionary
-  ## Information for Recognized Food (name/type)
-  ## Current Freshness of Food
-  ## Ambient Temperature of Demo Environment
-  ## Ambient Humidity of Demo Environment
-  ## Temperature Threshold Flag - The transmitter is outputting the light signals for the environment if the temperature lies outside of 30% of the standard room temperature range (“Temperature Threshold Flag”).
-  ## Humidity Threshold Flag - The transmitter is outputting the light signals for the environment if the humidity lies outside of 30% of the standard room humidity range (“Humidity Threshold Flag”).
-  ## return data
+  try:
+    ## formatting the data into a JSON -> work with dictionary
+    ## Information for Recognized Food (name/type)
+    ## Current Freshness of Food
+    ## Ambient Temperature of Demo Environment
+    ## Ambient Humidity of Demo Environment
+    ## Temperature Threshold Flag - The transmitter is outputting the light signals for the environment if the temperature lies outside of 30% of the standard room temperature range (“Temperature Threshold Flag”).
+    ## Humidity Threshold Flag - The transmitter is outputting the light signals for the environment if the humidity lies outside of 30% of the standard room humidity range (“Humidity Threshold Flag”).
+    ## return data
+  except:
+    print("Unexpected error:", sys.exc_info())
+    return -1
 
 def piTFT_disp():
   ## pygame
