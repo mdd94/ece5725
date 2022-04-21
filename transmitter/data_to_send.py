7bf6938876ec3fe03553f41200e941cfd0224eb7
Michelle Davies <mdd94@cornell.edu>
2022-04-21 13:21:17 -0400

    Update data_to_send.py
    DHT11 setup

--- transmitter/data_to_send.py
+++ transmitter/data_to_send.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # function to read, format, and return the sensor data that we need
 
 ## Import modules (gpio, libraries for reading data from sensors, etc)
@@ -10,6 +11,7 @@ import pygame
 from pygame.locals import *
 import os
 import subprocess
+import adafruit_dht
 
 ## Set up GPIO pins
 calibration_light_pin = 1
@@ -23,7 +25,7 @@ GPIO.setup(barcodes_pin, GPIO.IN)
 GPIO.setup(signal_light_pin[0], GPIO.OUT)
 GPIO.setup(signal_light_pin[1], GPIO.OUT)
 GPIO.setup(signal_light_pin[2], GPIO.OUT)
-
+dht11_device = adafruit_dht.DHT11(dht11_pin, use_pulseio=False)
 
 def calibration_light():
   ## The transmitter is outputting a calibration light signal to indicate that data is being transmitted to the receiver.
@@ -33,6 +35,7 @@ def camera_scanner():
   
 def temp_and_hum_capture():
   ## return values
+  
   
 
 def captureData():
