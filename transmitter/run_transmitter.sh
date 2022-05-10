e6f071ac737bd0fa2b6a57f5bc42deb6fd47525f
Michelle Davies <mdd94@cornell.edu>
2022-05-10 18:57:10 -0400

    Update run_transmitter.sh

--- transmitter/run_transmitter.sh
+++ transmitter/run_transmitter.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+#The background processes will occur below (e.g., transmission of data to receiver device(s))
+
+
+
+
+#The foreground processes will occur below (e.g., displaying data in RT on the PiTFT screen)
+
+
+
