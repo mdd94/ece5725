6bab94b64359f1dcd5b5e1bd215947db48ec077c
Michelle Davies <mdd94@cornell.edu>
2022-05-10 19:00:28 -0400

    Update run_transmitter.sh

--- transmitter/run_transmitter.sh
+++ transmitter/run_transmitter.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#In order to avoid disrupting the system, we will first ensure that all system dependencies are installed, and install them if not, before proceeding.
+
+
+
 #The background processes will occur below (e.g., transmission of data to receiver device(s))
 
 
