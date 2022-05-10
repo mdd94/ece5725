f3712a46e26dec62be223ce575809d4d10b83492
Michelle Davies <mdd94@cornell.edu>
2022-05-10 18:38:12 -0400

    Update main.py
    # transmitting the data will be run in background in the main batch file rather than here to not have the PiTFT process block it.
    This file main.py will take care of the data display on the transmitter (pi) side.

--- transmitter/main.py
+++ transmitter/main.py
@@ -1,8 +1,24 @@
 '''
 
-Transmitter Implementation - ECE 5725, W grp 3
+Transmitter Main Screen Implementation - ECE 5725, W grp 3
+# transmitting the data will be run in background in the main batch file rather than here to not have the PiTFT process block it.
+This file main.py will take care of the data display on the transmitter (pi) side.
 
 '''
 import time
 import data_to_send # import file with sensor and display functions defined
 import sys
+import RPi.GPIO as GPIO
+
+# quit btn - consistent with data_to_send.py
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+def gpio17_cb(channel):
+    GPIO.cleanup()
+    quit()
+GPIO.add_event_detect(17, GPIO.FALLING, callback=gpio17_cb, bouncetime=400)
+
+# main loop
+while True:
+  current_data = data_to_send.captureData() # this data capture will be shown in Real Time on the PiTFT. Because of issues with blocking the system for transmission for too long, the data transmission with be done in a separate instance as a background processes. However, we expect that as the environment remains fairly constant, the system data at the receiver will be fairly consistent.
+  # load the data unto the PiTFT
+  data_to_send.piTFT_disp(current_data)
