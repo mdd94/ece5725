2923af2ee49f5709f6ce3bb9870b1fe01d41e46c
Michelle Davies <mdd94@cornell.edu>
2022-05-19 12:51:56 -0400

    Update reciever.py

--- receiver/reciever.py
+++ receiver/reciever.py
@@ -12,17 +12,19 @@ data = json.load(f)
 print(f.read())
 
 # food inventory information {info}
-information = 
+information = get_data.inventory(data['info'])
 
 # temperature and humidity data
-temperature = 
-humidity = 
+temperature_c = get_data.temp_data(data['temp_c'])
+temperature_f = get_data.temp_data(data['temp_f'])
+humidity = get_data.hum_data(data['humidity'])
 
 # freshness of the food
-freshness = 
+freshness = get_data.fresh_data(data['freshness'])
 
 # recipe book data
-recipe = 
+r_in = get_data.websc_recipes(data['info'])
+recipe = get_data.recipe_book(r_in)
 
 # dashboard construction for date
 info = [information, temperature, humidity, freshness, recipe]
@@ -33,20 +35,23 @@ f_all = open("data_all.json", "r")
 data_all = json.load(f_all)
 print(f_all.read())
 i_info = []
-i_temp = []
+i_temp_c = []
+i_temp_f = []
 i_humi = []
 i_fresh = []
 i_rep = []
+timei = []
 for entries in data_all['data_string']:
   print(entries)
   if (entries['info'] != -1 and entries['temp_c'] != -1 and entries['temp_f'] != -1 and entries['humidity'] != -1 and entries['freshness'] != -1):
-    info = i_info.append(entries['info'])
-    temperature_c = i_temp.append(entries['temp_c'])
-    temperature_f = i_temp.append(entries['temp_f'])
-    humidity = i_humi.append(entries['humidity'])
-    freshness = i_fresh.append(entries['freshness'])
-    recipe = get_data.recipe_book(entries['info'])
+    i_info.append(entries['info'])
+    i_temp_c.append(entries['temp_c'])
+    i_temp_f.append(entries['temp_f'])
+    i_humi.append(entries['humidity'])
+    i_fresh.append(entries['freshness'])
+    recipe = get_data.recipe_book(get_data.websc_recipes(entries['info'][0]))
     i_rep.append(recipe)
+    timei.append(entries['timestamp_packet'])
 all_datapg = open("historical_data.html","w")
 pg = "<!DOCTYPE html> \n<html> \n<head> \n<title>Food Mgmt Dashboard</title> \n<style> \n.all-browsers {margin: 0; padding: 5px; background-color: rgb(240, 250, 255);} \n.all-browsers > h1, \n.browser {margin: 10px;  padding: 5px;} \n.browser {background: white;} \n.browser > h2, p {  margin: 4px;  font-size: 90%;} \nfooter { text-align: center; padding: 3px; background-color: lightgray; color: white;}\n</style>\n</head> <body>\n"
 pg += "<p>Welcome to the IoT Food Management System! Data is graphed below.</p>\n"
@@ -54,18 +59,26 @@ pg += "<h2> Food Inventory Log </h2>\n"
 for i in i_info:
   pg += "<p>{info_a}</p>\n".format(info_a=i)
 pg += "<h2> Temperature Graph over Time </h2>"
-plt.plot()
+plt.plot(timei, i_temp_c)
+plt.ylabel('Temp (C)')
+plt.xlabel('Entries over time')
 plt.savefig('temperature_c.png')
-plt.plot()
+plt.plot(timei, i_temp_f)
+plt.ylabel('Temp (F)')
+plt.xlabel('Entries over time')
 plt.savefig('temperature_f.png')
 pg += "<p><img src='temperature_c.png'></p>"
 pg += "<p><img src='temperature_f.png'></p>"
 pg += "<h2> Humidity Graph over Time </h2>"
-plt.plot()
+plt.plot(timei, i_humi)
+plt.ylabel('Humidity (%)')
+plt.xlabel('Entries over time')
 plt.savefig('humidity.png')
 pg += "<p><img src='humidity.png'></p>"
 pg += "<h2> Freshness Graph over Time </h2>"
-plt.plot()
+plt.plot(timei, i_fresh)
+plt.ylabel('Freshness (%)')
+plt.xlabel('Entries over time')
 plt.savefig('freshness.png')
 pg += "<p><img src='freshness.png'></p>"
 for r in i_rep:
