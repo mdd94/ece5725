6b500fd67062fbaac8c9c9f4589acc3597d9d03e
Michelle Davies <mdd94@cornell.edu>
2022-05-20 12:26:18 -0400

    Update run_transmitter.sh

--- transmitter/run_transmitter.sh
+++ transmitter/run_transmitter.sh
@@ -43,6 +43,9 @@ host_port = "65432"
 sudo python3 $script_full_path/transmit_server.py $host_addr $host_port &
 sudo python3 $script_full_path/transmit_client.py $host_addr $host_port &
 
+# Backup to GitHub
+/home/pi/ece5725/push_changes.sh &
+
 #The foreground processes will occur below (e.g., displaying data in RT on the PiTFT screen)
 
 sudo python3 $script_full_path/main.py
